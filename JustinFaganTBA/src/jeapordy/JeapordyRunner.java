684ab9ee7d0c90f79e5ffc616a00783a6e0c80da
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-10-25 17:09:14 -0400

    Brainstorm

--- JustinFaganTBA/src/jeapordy/JeapordyRunner.java
+++ JustinFaganTBA/src/jeapordy/JeapordyRunner.java
@@ -6,7 +6,8 @@ public class JeapordyRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
 	}
 	int playerBalance=0;
 	int numOfCorrect=0;
